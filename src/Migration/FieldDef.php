37731d9538cac55da565ee253fb525b5a5baa24d
Oliver Etchebarne <yo@drmad.org>
2021-09-22 10:42:34 -0500

    Add comments for some properties. Remove unnecesary 'null' property

--- src/Migration/FieldDef.php
+++ src/Migration/FieldDef.php
@@ -11,8 +11,16 @@ class FieldDef extends DataContainer
 {
     public string $name;
     public FieldType $type;
+
+    /** Lenght for Char, FixChar and Decimal (precision) fields*/
     public ?int $length = null;
+
+    /** Decimal digits (scale), for Decimal field */
     public ?int $decimal = null;
-    public bool $null = true;
+
+    /** Value list, for Enum field */
+    public ?array $values = null;
+
+    public bool $null = false;
     public $default = null;
 }
