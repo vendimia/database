a9c91259c53371fa3f22abeb1033b08656e8401b
Oliver Etchebarne <yo@drmad.org>
2022-02-15 02:39:04 -0500

    Add extra control over creating new tables
    $if_not_exists uses CREATE TABLE IF NOT EXISTS to create a table
    $drop_first executes a DROP TABLE IF EXISTS before creating

--- src/Migration/Action/Create.php
+++ src/Migration/Action/Create.php
@@ -11,14 +11,27 @@ use Attribute;
 #[Attribute]
 class Create implements ActionInterface
 {
+    public function __construct(
+        private $if_not_exists = false,
+        private $drop_first = false,
+    )
+    {
+
+    }
 
     public function perform(
         ConnectorInterface $connection,
         Schema $schema
     )
     {
+        if ($this->drop_first) {
+            $connection->execute('DROP TABLE IF EXISTS ' .
+                $connection->escapeIdentifier($schema->getTableName())
+            );
+        }
+
         // Empezamos por la tabla
-        $create = 'CREATE TABLE ' .
+        $create = 'CREATE TABLE ' . ($this->if_not_exists ? 'IF NOT EXISTS ' : '') .
             $connection->escapeIdentifier($schema->getTableName()) .
             " (" . $schema->getFieldsForCreate() . ')';
 
