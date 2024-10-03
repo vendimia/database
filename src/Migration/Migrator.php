0c1a43f3805a9bf44d7b8321af029499e752a4f8
Oliver Etchebarne <yo@drmad.org>
2024-10-03 16:27:42 -0500

    Improves the Migrator, optionally logging each activity

--- src/Migration/Migrator.php
+++ src/Migration/Migrator.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace Vendimia\Database\Migration;
 
 use Vendimia\Database\Driver\ConnectorInterface;
+use Vendimia\Helper\TextFromDocComment;
+use Vendimia\Logger\Logger;
 use ReflectionClass;
 use ReflectionAttribute;
 
@@ -11,7 +14,8 @@ use ReflectionAttribute;
 class Migrator
 {
     public function __construct(
-        private ConnectorInterface $connection
+        private ConnectorInterface $connection,
+        private ?Logger $logger = null,
     )
     {
 
@@ -20,7 +24,7 @@ class Migrator
     /**
      * Reads a migration file, and executes the migration
      */
-    public function execute($filename)
+    public function execute(string $filename): void
     {
         $class = require $filename;
 
@@ -28,6 +32,13 @@ class Migrator
 
         // Los métodos son las tablas
         $rc = new ReflectionClass($class);
+
+        // Sacamos la descripción
+        $description = explode("\n",
+            (string)new TextFromDocComment($rc->getDocComment())
+        )[0];
+
+        $this->logger?->info($description);
 
         foreach ($rc->getMethods() as $rm) {
             // El método debe tener una Action
@@ -49,23 +60,24 @@ class Migrator
             // Ejecutamos el hook 'pre'
             $pre_method = $table_name . '__pre_' . $action_name;
             if (method_exists($migration, $pre_method)) {
+                $this->logger?->info("· {$table_name}: {$action_name} (pre-hook)");
                 $migration->$pre_method();
             }
 
             // Creamos un schema, y ejecutamos el método
+            $this->logger?->info("· {$table_name}: {$action_name}");
             $schema = new Schema($table_name);
             $rm->invoke($migration, $schema);
 
             // Ejecutamos la acción
             $action->perform($this->connection, $schema);
 
-            // Y ejecutamos el hook 'pre'
-            $pre_method = $table_name . '__post_' . $action_name;
-            if (method_exists($migration, $pre_method)) {
-                $migration->$pre_method();
+            // Y ejecutamos el hook 'post'
+            $post_method = $table_name . '__post_' . $action_name;
+            if (method_exists($migration, $post_method)) {
+                $this->logger?->info("· {$table_name}: {$action_name} (post-hook)");
+                $migration->$post_method();
             }
-
         }
-
     }
 }
