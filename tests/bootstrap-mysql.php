c761646daa41acd0718a60e38ede5b0fd90b1e61
Oliver Etchebarne <yo@drmad.org>
2024-11-19 23:18:00 -0500

    Sets a über secure password for MySQL testing

--- tests/bootstrap-mysql.php
+++ tests/bootstrap-mysql.php
@@ -5,8 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 Vendimia\Database\Setup::init(
     new Vendimia\Database\Driver\Mysql\Connector(
         hostname: 'localhost',
-        username: 'root',   // FIXME: Must be obtained via $_ENV
-        password: '',       // FIXME: Must be obtained via $_ENV
-        database: 'mysql',  // FIXME: Must be obtained via $_ENV
+        username: 'root',       // FIXME: Must be obtained via $_ENV
+        password: 'pikachu',    // FIXME: Must be obtained via $_ENV
+        database: 'mysql',      // FIXME: Must be obtained via $_ENV
     ),
 );
