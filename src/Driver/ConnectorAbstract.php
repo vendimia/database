db428e8872dba03811ebccebe42a940ed26f2184
Oliver Etchebarne <yo@drmad.org>
2025-10-21 20:24:32 -0500

    Temporally replaces `*` for `+` regexp for matching digits.
    With an asterisk, the regexp matches a single `-` as a digit. That
    asterisk is used to match _zero_ or more digits before a decimal point,
    so a value like `-.4` is accepted as number.
    
    But due the fact that the decimal point and all the decimal digits
    should be optional, it accepts a single `-` as a digit, causing this
    bug.
    
    This commit replaces the `*` for a `+` so always an integer part is
    required, thus invalidating this (frankly

--- src/Driver/ConnectorAbstract.php
+++ src/Driver/ConnectorAbstract.php
@@ -140,7 +140,7 @@ abstract class ConnectorAbstract
                 fn($value) => $this->escape($value, $quote_char, $force_quote_numbers),
                 $value
             );
-        } elseif ($value != '' && preg_match('/^[-+]?\d*(\.\d+)?$/', $value) === 1) {
+        } elseif ($value != '' && preg_match('/^[-+]?\d+(\.\d+)?$/', $value) === 1) {
             // Ya que los numeros /también/ son strings, procesamos los números
             // primero
 
