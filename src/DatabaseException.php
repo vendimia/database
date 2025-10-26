6dfef5c78b429a3e24b50669de721a9428f863a9
Oliver Etchebarne <yo@drmad.org>
2025-10-26 10:46:54 -0500

    Adds description of Database\DatabaseException

--- src/DatabaseException.php
+++ src/DatabaseException.php
@@ -1,9 +1,16 @@
 <?php
+
 namespace Vendimia\Database;
 
 use Exception;
 use Throwable;
 
+/**
+ * Exception for database errors. Include an 'extra' field used for SQL query.
+ *
+ * This is the same as Vendimia\Exception\VendimiaException found in package
+ * vendimia/core, but repeated here just for decoupling purposes.
+ */
 class DatabaseException extends Exception
 {
     public function __construct(
@@ -12,7 +19,7 @@ class DatabaseException extends Exception
         ?Throwable $previous = null,
 
         /** Extra information for this exception */
-        private array $extra = [],
+        protected array $extra = [],
     )
     {
         parent::__construct($message, $code, $previous);
